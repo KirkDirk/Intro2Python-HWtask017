9adedcadc2c094d64b3931447e3b83a6afce8154
KirkDirk <jackalex@ya.ru>
2022-10-07 17:32:20 +0500

    add code

--- HWtask17.py
+++ HWtask17.py
@@ -3,3 +3,9 @@
 # список простых множителей числа N.
 
 somenum = int(input('Введите натуральное число N: '))
+somelist = []
+for i in range(2, somenum+1):
+    while somenum % i == 0 and i <= somenum: 
+        somelist.append(i)
+        somenum /= i
+print(somelist)
